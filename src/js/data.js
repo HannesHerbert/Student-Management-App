3fab741d09f372839f8cda206f664bb3d277a60a
Martin <martindrus@outlook.de>
2022-12-02 17:05:05 +0100

    added: delete & add fkt 4 students

--- src/js/data.js
+++ src/js/data.js
@@ -11,20 +11,71 @@ async function fetchStudents(limit, skip, classId) {
     if(skip === undefined) skip = "";
     if(classId === undefined) classId = "";
 
-
     response = await fetch(`https://test.100best.guide/locations/dci-students/student?limit=${limit}&skip=${skip}&classId=${classId}`);
     data = response.json();
 
-
     if (response.ok) {
-
         return Promise.resolve(data);
-    
     } else {
-
         return Promise.reject(data);
     }
-   
 }
 
-export { fetchStudents };
+
+// API-Kommunikations-Funktion zum Einreichen eines neuen Todos
+// erhaelt den Todotext als Parameter
+async function addNewStudent(newStudent) {
+    // Bilde Options-Objekt fuer fetch
+    let options = {
+        // HTTP Methode
+        method: 'POST',
+        headers: {
+            // Angabe der Form des Bodys (JSON)
+            'Content-Type': 'application/json'
+        },
+        // Der gesendete Body (auch Payload genannt)
+        body: JSON.stringify(newStudent)
+    };
+
+    let res = await fetch(`https://test.100best.guide/locations/dci-students/student`, options);
+    let body = await res.json();
+
+    console.log(res.status);
+
+    // Wenn POST-Antwort Statuscode 2XX hat
+    if (res.ok) {
+        // Loese den Promise positiv auf und gebe die Payload vom Server zurueck
+        console.log();
+        return Promise.resolve(body);
+    
+    } else if (res.status === 409) {
+        console.log("student bereits vorhanden");
+    } else {
+
+        return Promise.reject('fehler');
+    }
+
+}
+
+async function deleteStudent(studentId) {
+    let options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    let response = await fetch(`https://test.100best.guide/locations/dci-students/student/${studentId}`, options);
+    let body = await response.json();
+
+    if (response.status === 200) {
+        console.log("deleted Student");
+        return Promise.resolve(body);
+    } else {
+        console.log("not deleted, error");
+        return Promise.reject(body);
+    }
+
+}
+
+export { fetchStudents, addNewStudent, deleteStudent };
--- src/js/main.js
+++ src/js/main.js
@@ -5,33 +5,91 @@ import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 
 // Import our custom JS
-import { fetchStudents } from './data.js';
+import { fetchStudents, addNewStudent, deleteStudent } from './data.js';
 /* -------------------------------------------------------------------------------  */
 
-async function getData() {
- let dataSet = await fetchStudents();
+let dataSet1 = await fetchStudents();
 
+console.log(dataSet1);
+
+
+let data = {
+    address: {
+       street: "Übelst-Str",
+       streetNum: 3,
+       postalCode: 94315,
+       city: "Hoierswerda",
+    },
+    name: "Heribert Pappenheimer",
+    classId: "CS-2022",                               
+ }
+
+
+
+
+
+
+
+//let testAdd = await addNewStudent(data);
+
+//let testDelete = await deleteStudent("638a0f3e14ad8fee27c12ab9")
+
+async function getData() {
+
+    //fetching data fom API
+    let dataSet = await fetchStudents();
+
+    //creating a new Map
     let dataMap = new Map();
 
+    //Iterating over all Students
     dataSet.students.forEach(async student => {
+        //Extracting class name of every student 
         let className = student.classId;
+        //
         if (dataMap.has(className)) {
             let theClass = dataMap.get(className);
             theClass.push(student);
             dataMap.set(className, theClass);
         } else {
-            
             let classArray = [student];
             dataMap.set(className, classArray);
         }
     });  
-
     return dataMap
 }
 
+let map = await getData();
+console.log(map);
+
+async function renderMainPage(amountOfShownEntries, currentPage) {
+    let skip = 0;
+
+    skip = amountOfShownEntries * currentPage;
 
 
-console.log( );
+    if (currentPage > 1) {
+    }
+
+    console.log(skip);
+
+    // limit, skip, classId
+    let dataSet = await fetchStudents(amountOfShownEntries, skip, "");
+
+    // console.log(dataSet);
+    
+}
+
+// renderMainPage(5, 1)
+
+
+
+
+
+function sortArrayByAlphabet(unsortedArray) {
+    
+}
+
 
 
 
