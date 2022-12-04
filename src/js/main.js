0a93d402c34afff8d642bd2a01abc5ccb5425fec
Martin <martindrus@outlook.degit config --global user.name Martingit config --global user.email martindrus@outlook.de>
2022-12-04 14:57:16 +0100

    -

--- src/js/main.js
+++ src/js/main.js
@@ -67,44 +67,16 @@ async function getData() {
     return dataMap
 }
 
-let map = await getData();
-console.log(map);
+let map1 = await getData();
+let map = new Map([...map1].sort());
+// const map = new Map([...temp].sort((a, b) => console.log(a[1],b[1])));
 
-function renderClassNames() {
-    //Extrahiere KlassenStrings
-    let classNames = Array.from(map.keys());
-    classNames.forEach(className => {
-        let classOption = document.createElement("option");
-        classOption.value = className;
-        elClassSelectionInput.appendChild(classOption)
-    });
-}
-//renderClassNames();
+// map = new Map([...temp.entries()].sort((a, b) => a[1][0].name.localeCompare(b[1][0].name)));
+
+console.log(map);
 
 
 function renderAccordionItems() {
-    // elAccordion.replaceChildren();
-    /* 
-        <div class="accordion-item"> ok
-          <h2 class="accordion-header" id="headingOne"> ok
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-              aria-expanded="true" aria-controls="collapseOne">
-              Accordion Item #1
-            </button>
-          </h2>
-          <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
-              plugin
-              adds the appropriate classes that we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-    */
     let index = 1;
 
     for (const [key, value] of map) {
